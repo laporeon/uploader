0f63dea508dd33670062f5d9b83aabba1add3df8
laporeon <175569928+laporeon@users.noreply.github.com>
2026-03-05 15:58:12 -0300

    feat(exceptions): create globalhandlerexception and filestorageexception

---
+++ src/main/java/com/laporeon/uploader/exceptions/FileStorageException.java
@@ -0,0 +1,9 @@
+package com.laporeon.uploader.exceptions;
+
+public class FileStorageException extends RuntimeException {
+
+    public FileStorageException(String message) {
+        super(message);
+    }
+
+}
---
+++ src/main/java/com/laporeon/uploader/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,58 @@
+package com.laporeon.uploader.exceptions;
+
+import com.laporeon.uploader.dto.response.ErrorResponseDTO;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
+
+import java.time.Instant;
+
+@RestControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(FileStorageException.class)
+    public ResponseEntity<ErrorResponseDTO> handleFileStorageException(FileStorageException ex) {
+        ErrorResponseDTO error = new ErrorResponseDTO(
+                Instant.now(),
+                HttpStatus.BAD_REQUEST.value(),
+                "FILE_STORAGE_EXCEPTION",
+                ex.getMessage()
+        );
+
+        return ResponseEntity.badRequest().body(error);
+    }
+
+    @ExceptionHandler(MaxUploadSizeExceededException.class)
+    public ResponseEntity<ErrorResponseDTO> handleMaxUploadSize(MaxUploadSizeExceededException ex) {
+
+        ErrorResponseDTO error = new ErrorResponseDTO(
+                Instant.now(),
+                HttpStatus.CONTENT_TOO_LARGE.value(),
+                "MAX_UPLOAD_SIZE_EXCEEDED_ERROR",
+                "File size exceeded the upload limit"
+        );
+        return ResponseEntity.status(HttpStatus.CONTENT_TOO_LARGE).body(error);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponseDTO> handleException(Exception ex) {
+        log.error("An unexpected error occurred | exception={} | message={}",
+                ex.getClass().getSimpleName(),
+                ex.getMessage(),
+                ex);
+
+        ErrorResponseDTO error = new ErrorResponseDTO(
+                Instant.now(),
+                HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                "INTERNAL_SERVER_ERROR",
+                "An unexpected error occurred"
+        );
+
+        return ResponseEntity.internalServerError().body(error);
+    }
+
+}
