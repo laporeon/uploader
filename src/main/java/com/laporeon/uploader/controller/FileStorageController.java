cef13dc80e90fa82e2890b0c40d9c14cf9b4e27c
laporeon <175569928+laporeon@users.noreply.github.com>
2026-03-05 15:57:23 -0300

    feat(api): create method to upload file on controller layer

--- src/main/java/com/laporeon/uploader/controller/FileStorageController.java
+++ src/main/java/com/laporeon/uploader/controller/FileStorageController.java
@@ -1,32 +1,27 @@
 package com.laporeon.uploader.controller;
 
 import com.laporeon.uploader.dto.response.FileUploadResponseDTO;
-import com.laporeon.uploader.service.FileUploadService;
+import com.laporeon.uploader.service.FileStorageService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+
 @RestController
 @RequestMapping("/api/v1/files")
 @RequiredArgsConstructor
-public class FileUploadController {
+public class FileStorageController {
 
-    private final FileUploadService fileUploadService;
+    private final FileStorageService fileStorageService;
 
-    @GetMapping()
-    public String uploadFile() {
-        return "Hello, World!";
-    }
-
-    @PostMapping(consumes = {MediaType.MULTIPART_FORM_DATA_VALUE}, produces = {MediaType.APPLICATION_JSON_VALUE})
+    @PostMapping()
     public ResponseEntity<FileUploadResponseDTO> uploadFile(
-            @RequestParam("file")MultipartFile file,
-            @RequestParam("name") String name
-    ) {
-        FileUploadResponseDTO fileUploadResponseDTO = fileUploadService.upload(file, name);
+            @RequestParam("file")MultipartFile file
+    ) throws IOException {
+        FileUploadResponseDTO fileUploadResponseDTO = fileStorageService.upload(file);
         return ResponseEntity.status(HttpStatus.CREATED).body(fileUploadResponseDTO);
     }
 }
