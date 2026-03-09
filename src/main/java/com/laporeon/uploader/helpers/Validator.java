f0eec6a2883d662b3f250c044b02b1c0df072e35
laporeon <175569928+laporeon@users.noreply.github.com>
2026-03-09 14:41:32 -0300

    feat(api): create method to validate filename using custom regex

--- src/main/java/com/laporeon/uploader/helpers/Validator.java
+++ src/main/java/com/laporeon/uploader/helpers/Validator.java
@@ -1,6 +1,6 @@
 package com.laporeon.uploader.helpers;
 
-import com.laporeon.uploader.exceptions.custom.InvalidFileExtensionException;
+import com.laporeon.uploader.exceptions.custom.InvalidFileNameException;
 
 import java.util.List;
 
@@ -12,14 +12,29 @@ public class Validator {
             "mp3", "wav", "ogg", "flac", "mp4", "avi", "mov", "mkv", "webm"
     );
 
+    private static final String FILENAME_REGEX_VALIDATOR =
+            "^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}\\." +
+                    "(jpg|jpeg|png|svg|gif|webp|bmp|ico|" +
+                    "pdf|txt|doc|docx|xls|xlsx|ppt|pptx|csv|" +
+                    "mp3|wav|ogg|flac|mp4|avi|mov|mkv|webm)$";
+
     public static String validateFileExtension(String originalFileName) {
         int fileExtensionIndex = originalFileName.lastIndexOf('.');
         String fileExtension = originalFileName.substring(fileExtensionIndex + 1).toLowerCase();
 
         if (fileExtensionIndex == -1 || !ALLOWED_FILE_EXTENSIONS.contains(fileExtension)) {
-            throw new InvalidFileExtensionException();
+            throw new InvalidFileNameException("Invalid or missing file extension");
         }
 
         return fileExtension;
+    }
+
+    public static void validateFileName(String fileName) {
+        String invalidFileNameMessage = "Invalid file name: '%s'. Expected format: UUID followed by an allowed extension " +
+                "(e.g., '123e4567-e89b-12d3-a456-426614174000.pdf').";
+
+        if (!fileName.matches(FILENAME_REGEX_VALIDATOR)) {
+            throw new InvalidFileNameException(invalidFileNameMessage.formatted(fileName));
+        }
     }
 }
