3adcfc9b78b452f67acb6da350191bd8e36c49a9
laporeon <175569928+laporeon@users.noreply.github.com>
2026-03-09 15:41:31 -0300

    refactor(helpers): improve naming consistency on constants

--- src/main/java/com/laporeon/uploader/helpers/Validator.java
+++ src/main/java/com/laporeon/uploader/helpers/Validator.java
@@ -12,11 +12,14 @@ public class Validator {
             "mp3", "wav", "ogg", "flac", "mp4", "avi", "mov", "mkv", "webm"
     );
 
-    private static final String FILENAME_REGEX_VALIDATOR =
+    private static final String FILENAME_PATTERN =
             "^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}\\." +
                     "(jpg|jpeg|png|svg|gif|webp|bmp|ico|" +
                     "pdf|txt|doc|docx|xls|xlsx|ppt|pptx|csv|" +
                     "mp3|wav|ogg|flac|mp4|avi|mov|mkv|webm)$";
+
+    private static final String INVALID_FILENAME_MESSAGE =  "Invalid file name: '%s'. Expected format: UUID followed by " +
+            "an allowed extension (e.g., '123e4567-e89b-12d3-a456-426614174000.pdf').";
 
     public static String validateFileExtension(String originalFileName) {
         int fileExtensionIndex = originalFileName.lastIndexOf('.');
@@ -30,11 +33,10 @@ public class Validator {
     }
 
     public static void validateFileName(String fileName) {
-        String invalidFileNameMessage = "Invalid file name: '%s'. Expected format: UUID followed by an allowed extension " +
-                "(e.g., '123e4567-e89b-12d3-a456-426614174000.pdf').";
 
-        if (!fileName.matches(FILENAME_REGEX_VALIDATOR)) {
-            throw new InvalidFileNameException(invalidFileNameMessage.formatted(fileName));
+        if (!fileName.matches(FILENAME_PATTERN)) {
+            throw new InvalidFileNameException(INVALID_FILENAME_MESSAGE.formatted(fileName));
         }
+
     }
 }
